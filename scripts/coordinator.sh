83b8ef588d6311ae124414f542bdad15bd9ff1fd
siscia <simone@mweb.biz>
2016-03-02 23:24:08 +0100

    dump files

--- scripts/coordinator.sh
+++ scripts/coordinator.sh
@@ -1,8 +1,10 @@
 for replication in 1 2 3 4; do
-  for blocksize in 1048576 1572864 2097152; do
+  for blocksize in 16640000 33280000 99840000 ; do
     for dimension in 200000000 300000000 400000000 500000000; do
-      echo $(printf "Running test with: \n%s %s %s" $replication $blocksize $dimension) >> log_test.txt
-      ./run_test.sh $replication $blocksize $dimension
+      for percentage in 0.5 0.05; do
+        echo $(printf "Running test with: \n%s %s %s %s" $replication $blocksize $dimension $percentage) >> log_test.txt
+        ./run_test.sh $replication $blocksize $dimension $percentage
+      done;
     done;
   done;
 done;
--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -3,10 +3,11 @@
 replication="$1"
 blocksize="$2"
 dimension="$3"
+completedmaps="$4"
 
 HADOOP_DIR="/opt/hadoop-3.0.0-SNAPSHOT/share/hadoop/mapreduce"
 
-dir=$(printf "%s_%s_%s" $replication $blocksize $dimension)
+dir=$(printf "%s_%s_%s_%s" $replication $blocksize $dimension $completedmaps)
 
 hadoop \
 	jar $HADOOP_DIR/hadoop-mapreduce-examples-3.0.0-SNAPSHOT.jar \
@@ -15,11 +16,11 @@ hadoop \
 	-Ddfs.replication="$replication" \
 	-Dmapred.map.tasks=50 \
 	"$dimension" \
-	$dir | tee $(printf "/root/marc_log/teragen/%s_%s_%s.txt" $replication $blocksize $dimension)
+	$dir | tee $(printf "/root/marc_log/teragen/%s_%s_%s_%s.txt" $replication $blocksize $dimension $completedmaps)
 
 sleep 30
 
-log_file=$(printf "marc_log/%s_%s_%s.csv" $replication $blocksize $dimension)
+log_file=$(printf "marc_log/%s_%s_%s_%s.csv" $replication $blocksize $dimension $completedmaps)
 
 ssh root@clovertown00 ./dstat-daemon.sh start $log_file
 ssh root@clovertown01 ./dstat-daemon.sh start $log_file
@@ -34,8 +35,9 @@ hadoop \
 	jar $HADOOP_DIR/hadoop-mapreduce-examples-3.0.0-SNAPSHOT.jar \
 	terasort \
 	-Dmapred.reduce.tasks=12 \
+	-Dmapreduce.job.reduce.slowstart.completedmaps="$completedmaps" \
 	$dir \
-	$result_dir | tee $(printf "/root/marc_log/terasort/%s_%s_%s.txt" $replication $blocksize $dimension)
+	$result_dir | tee $(printf "/root/marc_log/terasort/%s_%s_%s_%s.txt" $replication $blocksize $dimension $completedmaps)
 
 sleep 30
 
